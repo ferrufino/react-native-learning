7c8a3e233cb35754cfa21a15be9befe12efc6293
ferrufino <gustavo.fdlf@gmail.com>
2017-05-18 19:10:19 -0400

    Adds Taylor Swift Album app

---
+++ albums/index.ios.js
@@ -0,0 +1,23 @@
+/*
+
+iOS JS
+
+*/
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => {
+
+  return (
+      <View style={{ flex: 1}}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+    );
+ 
+};
+
+AppRegistry.registerComponent('albums', () => App);
